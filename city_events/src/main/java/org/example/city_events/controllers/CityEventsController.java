abe2ca0c27d2b3dcc742977c8e010582b612a096
danielasolomon <daniela.solomon06@gmail.com>
2025-12-17 22:43:15 +0200

    Postgres in cluster

--- city_events/src/main/java/org/example/city_events/controllers/CityEventsController.java
+++ city_events/src/main/java/org/example/city_events/controllers/CityEventsController.java
@@ -1,31 +1,51 @@
 package org.example.city_events.controllers;
 
+import org.example.city_events.repo.EventRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/events")
 public class CityEventsController {
-    record Event(Long id, String name, String city){};
+//    record Event(Long id, String name, String city){};
+//
+//    private final List<Event> events = List.of(
+//            new Event(1L,"Rock concert", "Bucharest"),
+//            new Event(2L,"Painting meetup", "Brasov"),
+//            new Event(3L,"Games night", "Medias")
+//    );
 
-    private final List<Event> events = List.of(
-            new Event(1L,"Rock concert", "Bucharest"),
-            new Event(2L,"Painting meetup", "Brasov"),
-            new Event(3L,"Games night", "Medias")
-    );
+    private final EventRepository eventRepository;
 
-    @GetMapping("/events")
+    public CityEventsController(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @GetMapping
     public List<Event> getEvents() {
-        return events;
+        return eventRepository.findAll();
     }
 
-    @GetMapping("/events/city/{city}")
+    @GetMapping("/city/{city}")
     public List<Event> getByCity(@PathVariable String city) {
-        return events.stream()
-                .filter(e ->e.city.equalsIgnoreCase(city))
-                .toList();
+        return eventRepository.findByCityIgnoreCase(city);
     }
+
+
+//    @GetMapping("/events")
+//    public List<Event> getEvents() {
+//        return events;
+//    }
+//
+//    @GetMapping("/events/city/{city}")
+//    public List<Event> getByCity(@PathVariable String city) {
+//        return events.stream()
+//                .filter(e ->e.city.equalsIgnoreCase(city))
+//                .toList();
+//    }
 
 }
---
+++ city_events/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO events (name, city) VALUES
+    ('Rock Festival', 'Bucharest'),
+    ('Jazz Night', 'Cluj'),
+    ('Tech Meetup', 'Bucharest');
